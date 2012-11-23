599d46b061a29605397b83cc24d57d2f14a77514
Nikolas Engelhard <nikolas.engelhard@in.tum.de>
2012-11-23 14:53:24 +0100

    now also building a libgeometry

--- install_linux.sh
+++ install_linux.sh
@@ -5,11 +5,10 @@ cd WildMagic4/LibFoundation
 make -f makefile.wm4 -j3 CFG=Release SYS=linux GRF=OpenGL
 
 
-echo 'Please edit CmakeLists.txt (GSI_FOLDER and GEO_FOLDER)'
+echo 'Please edit CMakeLists.txt (GSI_FOLDER and GEO_FOLDER)'
 
 exit
 
 cd ../../Testing
-mkdir build
 cmake .. && make -j3
 ./foo sphere.obj
